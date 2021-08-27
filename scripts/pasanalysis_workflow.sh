12a7840ce36e94c640dadbe96a1db01f30eb6259
Sereno Lopez-Darwin <sereno@princeton.edu>
2021-08-27 08:42:16 -0400

    Delete pasanalysis_workflow.sh

--- scripts/pasanalysis_workflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# These two lines (4, 5) is replaced by the head script.
-dataset="cutoff"
-wd="/net/shendure/vol1/home/sereno/projects/cell_clustering/nobackup/newannotations"
-scriptpath=/net/shendure/vol1/home/sereno/projects/scripts/
-
-Rscript ${scriptpath}differentialpasanalysis.R $dataset $wd ${SGE_TASK_ID}
